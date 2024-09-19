17ded31835886bc83f42a12abb678cfcbdd1a017
Tribog <julix4212@gmail.com>
2024-09-19 09:03:08 -0500

    feat: the ProductSeeder was created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JWT_ProductManager_Public2.Models;
+using JWT_ProductManager_Public2.seeders;
 using Microsoft.EntityFrameworkCore;
 
 namespace JWT_ProductManager_Public2.Data;
@@ -10,6 +11,12 @@ namespace JWT_ProductManager_Public2.Data;
 public class ApplicationDbContext : DbContext
 
 {
-    public DbSet<Product> Products{get; set;}
+    public DbSet<Product> Products { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        ProductSeeder.Seed(modelBuilder, 1000);
+    }
+
 }
---
+++ seeders/ProductSeeder.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using JWT_ProductManager_Public2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT_ProductManager_Public2.seeders;
+
+public class ProductSeeder
+{
+    public static void Seed(ModelBuilder modelBuilder, int amount)
+    {
+        var products = GenerateProducts(amount);
+        modelBuilder.Entity<Product>().HasData(products);
+    }
+
+    private static IEnumerable<Product> GenerateProducts(int count)
+    {
+        var faker = new Faker<Product>()
+            .RuleFor(p => p.Id, f => f.IndexFaker + 1)
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Cost, f => f.Random.Double(1, 1000));
+
+        return faker.Generate(count);
+    }
+}
+
